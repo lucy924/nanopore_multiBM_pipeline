8bbc6cf149c85d99a79abcffd5ac84fb76cf8cfc
lucy924 <lucypicard@outlook.com>
2025-11-16 13:04:12 +1300

    finished cleanup

--- development/managing_sequence_files/cleanup.sh
+++ development/managing_sequence_files/cleanup.sh
@@ -6,29 +6,18 @@
 # rsync --recursive --links --perms --times --human-readable --partial --progress --update --remove-source-files $source $dest
 
 
-source="/mnt/uow-pathology-dun/lucy/data/231031-LPFT-LSK114"
-dest="/mnt/uow-pathology-dun/hans_external/data/lucy/panorama/"
+# source="/mnt/uow-pathology-dun/lucy/data/231031-LPFT-LSK114"
+# dest="/mnt/uow-pathology-dun/hans_external/data/lucy/panorama/"
 
-rsync --recursive --links --perms --times --human-readable --partial --progress --update --remove-source-files $source $dest
+# rsync --recursive --links --perms --times --human-readable --partial --progress --update --remove-source-files $source $dest
 
-source="/mnt/uow-pathology-dun/lucy/data/231031-LPFT-LSK114"
-dest="/mnt/uow-pathology-dun/hans_external/data/lucy/panorama/"
+# source="/mnt/uow-pathology-dun/lucy/data/ESR/*"
+# dest="/mnt/uow-pathology-dun/hans_external/data/lucy/panorama/20240131-ESR_data/"
 
-rsync --recursive --links --perms --times --human-readable --partial --progress --update --remove-source-files $source $dest
+# rsync --recursive --links --perms --times --human-readable --partial --progress --update --remove-source-files $source $dest
+# mv /mnt/uow-pathology-dun/hans_external/data/lucy/panorama/20240131-ESR_data /mnt/uow-pathology-dun/hans_external/data/lucy/panorama/ESR
 
-source="/mnt/uow-pathology-dun/lucy/data/ESR/ESR_data_from_nesi/20240131-ESR.basecalls.bam"
-dest="/mnt/uow-pathology-dun/lucy/data/ESR/"
+# source="/mnt/uow-pathology-dun/lucy/data/231031-LPFT-LSK114/231031-LPFT-LSK114/*"
+# dest="/mnt/uow-pathology-dun/lucy/data/231031-LPFT-LSK114/"
 
-rsync --recursive --links --perms --times --human-readable --partial --progress --update --remove-source-files $source $dest
-
-source="/mnt/uow-pathology-dun/lucy/data/ESR/*"
-dest="/mnt/uow-pathology-dun/hans_external/data/lucy/panorama/20240131-ESR_data/"
-
-rsync --recursive --links --perms --times --human-readable --partial --progress --update --remove-source-files $source $dest
-mv 20240131-ESR_data ESR
-
-
-source="/mnt/uow-pathology-dun/lucy/data/231031-LPFT-LSK114/231031-LPFT-LSK114/*"
-dest="/mnt/uow-pathology-dun/lucy/data/231031-LPFT-LSK114/"
-
-rsync --recursive --links --perms --times --human-readable --partial --progress --update --remove-source-files $source $dest
+# rsync --recursive --links --perms --times --human-readable --partial --progress --update --remove-source-files $source $dest
